ce1bd352b499d85d7c9cae0f434041bdc13b54d2
Alexandro F. S. Martins <alexandrofsmartins@gmail.com>
2017-04-07 21:23:57 -0300

    Configuração Index Inicial Feita com Sucesso!

--- app/config.php
+++ app/config.php
@@ -2,6 +2,8 @@
 //Constantes
 $configs = new HXPHP\System\Configs\Config;
 
+$configs->title = 'Megavale Soluções - Painel';
+
 $configs->env->add('development');
 
 $configs->env->development->baseURI = '/http_painel/';
